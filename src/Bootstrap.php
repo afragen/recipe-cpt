fd5ab135b4c9003975d97a83d3b712b5c39253e9
Andy Fragen <andy@thefragens.com>
2019-12-26 15:04:47 -0800

    show CPTs on home page

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -17,6 +17,13 @@ class Bootstrap {
 	use Taxonomy;
 
 	/**
+	 * Array of Custom Post Types.
+	 *
+	 * @var array
+	 */
+	private $cpts = [];
+
+	/**
 	 * Load hook that registers CPTs and Taxonomies.
 	 *
 	 * @return void
@@ -25,12 +32,24 @@ class Bootstrap {
 		add_action(
 			'init',
 			function () {
-				$cpt_slug = $this->register_cpt( 'KJ recipe', 'KJ recipes', 'dashicons-cart' );
+				$this->cpts[] = $cpt_slug = $this->register_cpt( 'KJ recipe', 'KJ recipes', 'dashicons-cart' );
 
 				$this->register_tax( 'cuisine', 'cuisines', $cpt_slug );
 				$this->register_tax( 'course', 'courses', $cpt_slug );
 				$this->register_tax( 'ingredient', 'ingredients', $cpt_slug );
 				// $this->register_tax( 'sauce', 'sauces', $cpt_slug );
+			}
+		);
+
+		// Add CPTs to main blog page.
+		add_filter(
+			'pre_get_posts',
+			function( $query ) {
+				if ( is_home() && $query->is_main_query() ) {
+					// $query->set( 'post_type', array_merge( [ 'post' ], $this->cpts ) );
+				}
+
+				return $query;
 			}
 		);
 	}
